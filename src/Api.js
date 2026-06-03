6d9f5993a145fa4d7499a19cbd1799cd339fc854
LauraPratSala <lprsa18@gmail.com>
2026-06-03 19:29:13 +0200

    [fix] api.js

--- src/Api.js
+++ src/Api.js
@@ -6,8 +6,9 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('auth_token');
-  if (token) {
-    config.headers.Authorization = `Basic ${token}`;
+  const isPublicRoute = config.url?.startsWith('/auth/');
+  if (token && !isPublicRoute) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
